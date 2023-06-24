e4f7261b7dcdb9ae1083b949fea0482cb9b282a8
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2023-06-24 08:57:10 +0330

    Add files via upload

---
+++ power.py
@@ -0,0 +1,6 @@
+print("---------------------------------------")
+num1 = int(input("Please enter first  number : "))
+num2 = int(input("Please enter second number : "))
+powered_number = num1 ** num2
+print(num1, "powered by", num2, "is :", powered_number)
+print("---------------------------------------")
