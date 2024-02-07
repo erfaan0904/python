d83e45d76d19cbfc6850ac834398fd694e028a60
Erfan najafi <87322931+erfaan0904@users.noreply.github.com>
2024-02-07 11:59:09 +0330

    Add files via upload

---
+++ num 1 powered by num 2.py
@@ -0,0 +1,9 @@
+print("----------------------------------------------------")
+a = int(input("Enter 1nd numer : "))
+b = int(input("Enter 2nd numer : "))
+c = a ** b
+print("----------------------------------------------------")
+print("1nd number : ", a)
+print("2nd number : ", b)
+print("1nd number powered by 2nd number is : ", c)
+print("----------------------------------------------------")
